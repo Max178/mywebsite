9be6a4e5c4694a8a763d5d39ab1553977362a7bd
Max Pendse <maxpendse@Maxs-MacBook-Air-3.local>
2025-07-20 13:19:43 -0700

    First website stuff

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Max's Beautiful Wesbite
+      </header>
+      <main className='App-main'>
+        this is the content of my website
+      </main>
+    </div>
+  );
+}
+
+export default App;
